82adfa1933ef6111b7bed8247732dd8624bd94d1
talkingwoman <aisshesoboleva@yandex.ru>
2026-04-03 21:58:47 +0300

    Initial Commit

---
+++ ImageFeed/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  ImageFeed
+//
+//  Created by VIctoria Soboleva on 03.04.2026.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
